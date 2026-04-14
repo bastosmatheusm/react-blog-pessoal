930b298f53e1c81546f79211dd5c702e7e2930fc
bastosmatheusm <matheusm.bastos@hotmail.com>
2026-04-14 12:16:22 -0300

    ✨ New Feature: Consumo - Recurso Postagem, Perfil do Usuário e ajustes finos

--- src/App.tsx
+++ src/App.tsx
@@ -5,16 +5,23 @@ import Home from "./pages/home/Home"
 import Cadastro from "./pages/cadastro/Cadastro"
 import Login from "./pages/login/Login"
 import { AuthProvider } from "./contexts/AuthContext"
+import DeletarTema from "./components/tema/deletartema/DeletarTema"
 import FormTema from "./components/tema/formtema/FormTema"
 import ListaTemas from "./components/tema/listatemas/ListaTemas"
-import DeletarTema from "./components/tema/deletartema/DeletarTema"
-
+import DeletarPostagem from "./components/postagem/deletarpostagem/DeletarPostagem"
+import FormPostagem from "./components/postagem/formpostagem/FormPostagem"
+import AtualizarPerfil from "./pages/perfil/AtualizarPerfil"
+import Perfil from "./pages/perfil/Perfil"
+import { ToastContainer } from "react-toastify"
+import 'react-toastify/dist/ReactToastify.css'
+import ListaPostagens from "./components/postagem/listapostagem/ListaPostagem"
 
 function App() {
   
   return (
     <>
       <AuthProvider>
+        <ToastContainer />
         <BrowserRouter>
           <Navbar />
           <div className="min-h-[80vh]">
@@ -27,6 +34,12 @@ function App() {
 							<Route path="/cadastrartema" element={<FormTema />} />
 							<Route path="/editartema/:id" element={<FormTema />} />
 							<Route path="/deletartema/:id" element={<DeletarTema />} />
+              <Route path="/postagens" element={<ListaPostagens />} />
+							<Route path="/cadastrarpostagem" element={<FormPostagem />} />
+							<Route path="/editarpostagem/:id" element={<FormPostagem />} />
+							<Route path="/deletarpostagem/:id" element={<DeletarPostagem />} />
+              <Route path="/perfil" element={<Perfil />} />
+							<Route path="/atualizarusuario" element={<AtualizarPerfil />} />
             </Routes>
           </div>
           <Footer />
